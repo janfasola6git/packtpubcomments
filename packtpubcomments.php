be145c8f1a74f70c649c97724e26dfdb531c7e97
janfasola <odense123101@yahoo.dk>
2014-03-27 00:56:21 +0100

    gettpl
    sdf

--- packtpubcomments.php
+++ packtpubcomments.php
@@ -6,8 +6,18 @@ class PacktpubComments extends Module
 	{
 		$this->name = 'packtpubcomments';
 		$this->displayName = 'Packtpub comments';
-		parent::__construct();
 		$this->tab = 'front_office_features';
 		$this->version = '0.1';
+		$this->author = 'Fabien Serny';
+		$this->description = 'With this module, your customers will
+								be able to grade and comments your products.';
+		parent::__construct();
 	}
+	
+	public function getContent()
+	{
+		return $this->display(__FILE__, 'getContent.tpl');
+	}
+	
+	
 }
